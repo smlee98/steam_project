196e981572e436889367334c8cd8f4d382db282a
lee <user1@DESKTOP-ITMO77A>
2020-08-20 18:03:39 +0900

    First Commit (20/08/20_18:00)

---
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+	@RequestMapping(value="/main", method=RequestMethod.GET)
+	public String main() {
+		return "main";
+	}
+	
+	@RequestMapping(value="/main_user", method=RequestMethod.GET)
+	public String main_user() {
+		return "main_user";
+	}
+	
+	@RequestMapping(value="/mypage", method=RequestMethod.GET)
+	public String mypage() {
+		return "mypage";
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping(value="/register", method=RequestMethod.GET)
+	public String register() {
+		return "register";
+	}
+	
+	@RequestMapping(value="/game_1", method=RequestMethod.GET)
+	public String game_1() {
+		return "game_1";
+	}
+}
