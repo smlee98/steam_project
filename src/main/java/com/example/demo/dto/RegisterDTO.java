77664ebafab6db508ad8adc74cb046ce9ab050f9
lee <user1@DESKTOP-ITMO77A>
2020-08-29 23:37:00 +0900

    Release 8 (200829_2336)

--- src/main/java/com/example/demo/dto/RegisterDTO.java
+++ src/main/java/com/example/demo/dto/RegisterDTO.java
@@ -1,7 +1,6 @@
 package com.example.demo.dto;
 
 public class RegisterDTO {
-	
 	int number;
 	String id;
 	String password;
@@ -10,19 +9,16 @@ public class RegisterDTO {
 	String address;
 	String phone;
 	String favorite;
+	String role;
+	String auth;
 	
-	public RegisterDTO(int number, String id, String password, String name, String gender, String address, String phone,
-			String favorite) {
-		this.number = number;
+	// for login
+	public RegisterDTO(String id, String password) {
 		this.id = id;
 		this.password = password;
-		this.name = name;
-		this.gender = gender;
-		this.address = address;
-		this.phone = phone;
-		this.favorite = favorite;
 	}
 	
+	// for register
 	public RegisterDTO(String id, String password, String name, String gender, String address, String phone,
 			String favorite) {
 		this.id = id;
@@ -33,73 +29,120 @@ public class RegisterDTO {
 		this.phone = phone;
 		this.favorite = favorite;
 	}
+		
+	public RegisterDTO(int number, String id, String password, String name, String gender, String address, String phone,
+			String favorite, String role, String auth) {
+		this.number = number;
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.gender = gender;
+		this.address = address;
+		this.phone = phone;
+		this.favorite = favorite;
+		this.role = role;
+		this.auth = auth;
+	}
+	
+	public String getRole() {
+		return role;
+	}
 
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
+	public String getAuth() {
+		return auth;
+	}
+
+	public void setAuth(String auth) {
+		this.auth = auth;
+	}
+	
 	public int getNumber() {
 		return number;
 	}
+
 
 	public void setNumber(int number) {
 		this.number = number;
 	}
 
+
 	public String getId() {
 		return id;
 	}
+
 
 	public void setId(String id) {
 		this.id = id;
 	}
 
+
 	public String getPassword() {
 		return password;
 	}
+
 
 	public void setPassword(String password) {
 		this.password = password;
 	}
 
+
 	public String getName() {
 		return name;
 	}
+
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
+
 	public String getGender() {
 		return gender;
 	}
+
 
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
 
+
 	public String getAddress() {
 		return address;
 	}
+
 
 	public void setAddress(String address) {
 		this.address = address;
 	}
 
+
 	public String getPhone() {
 		return phone;
 	}
+
 
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
 
+
 	public String getFavorite() {
 		return favorite;
 	}
 
+
 	public void setFavorite(String favorite) {
 		this.favorite = favorite;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "RegisterDTO [number=" + number + ", id=" + id + ", password =" + password + ", name=" + name + ", gender=" + gender + ", address=" + address + ", phone=" + phone  + ", favorite=" + favorite + "]";
+		return "RegisterDTO [number=" + number + ", id=" + id + ", password=" + password + ", name=" + name
+				+ ", gender=" + gender + ", address=" + address + ", phone=" + phone + ", favorite=" + favorite + "]";
 	}
+	
 }
