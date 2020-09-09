818da6ca86d34a76450970d2f897ff3e426cbca5
smlee98 <smlee@gridone.co.kr>
2020-09-09 23:32:30 +0900

    Release 13 (200909_2332)

--- src/main/java/com/example/demo/dto/UploadDTO.java
+++ src/main/java/com/example/demo/dto/UploadDTO.java
@@ -1,8 +1,10 @@
 package com.example.demo.dto;
+import org.springframework.web.multipart.MultipartFile;
 
 public class UploadDTO {
 	int number;
-	String file;
+	String orgfile;
+	String newfile;
 	String thumbnail;
 	String name;
 	String category;
@@ -10,21 +12,24 @@ public class UploadDTO {
 	int amount;
 	String explain;
 	
-	public UploadDTO(String file, String thumbnail, String name, String category, String version, int amount,
-			String explain) {
-		this.file = file;
+	private MultipartFile files;
+	
+	public UploadDTO(String orgfile, String thumbnail, String name, String category, String version, int amount,
+			String explain, MultipartFile files) {
+		this.orgfile = orgfile;
 		this.thumbnail = thumbnail;
 		this.name = name;
 		this.category = category;
 		this.version = version;
 		this.amount = amount;
 		this.explain = explain;
+		this.files = files;
 	}
 	
-	public UploadDTO(int number, String file, String thumbnail, String name, String category, String version,
+	public UploadDTO(int number, String orgfile, String thumbnail, String name, String category, String version,
 			int amount, String explain) {
 		this.number = number;
-		this.file = file;
+		this.orgfile = orgfile;
 		this.thumbnail = thumbnail;
 		this.name = name;
 		this.category = category;
@@ -32,62 +37,92 @@ public class UploadDTO {
 		this.amount = amount;
 		this.explain = explain;
 	}
-	
+
 	public int getNumber() {
 		return number;
 	}
+
 	public void setNumber(int number) {
 		this.number = number;
 	}
-	public String getFile() {
-		return file;
+
+	public String getOrgfile() {
+		return orgfile;
 	}
-	public void setFile(String file) {
-		this.file = file;
+
+	public void setOrgfile(String orgfile) {
+		this.orgfile = orgfile;
 	}
+
+	public String getNewfile() {
+		return newfile;
+	}
+
+	public void setNewfile(String newfile) {
+		this.newfile = newfile;
+	}
+
 	public String getThumbnail() {
 		return thumbnail;
 	}
+
 	public void setThumbnail(String thumbnail) {
 		this.thumbnail = thumbnail;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getCategory() {
 		return category;
 	}
+
 	public void setCategory(String category) {
 		this.category = category;
 	}
+
 	public String getVersion() {
 		return version;
 	}
+
 	public void setVersion(String version) {
 		this.version = version;
 	}
+
 	public int getAmount() {
 		return amount;
 	}
+
 	public void setAmount(int amount) {
 		this.amount = amount;
 	}
+
 	public String getExplain() {
 		return explain;
 	}
+
 	public void setExplain(String explain) {
 		this.explain = explain;
 	}
-	
+
+	public MultipartFile getFiles() {
+		return files;
+	}
+
+	public void setFiles(MultipartFile files) {
+		this.files = files;
+	}
+
 	@Override
 	public String toString() {
-		return "UploadDTO [number=" + number + ", file=" + file + ", thumbnail=" + thumbnail + ", name=" + name
-				+ ", category=" + category + ", version=" + version + ", amount=" + amount + ", explain=" + explain
-				+ "]";
+		return "UploadDTO [number=" + number + ", orgfile=" + orgfile + ", newfile=" + newfile + ", thumbnail="
+				+ thumbnail + ", name=" + name + ", category=" + category + ", version=" + version + ", amount="
+				+ amount + ", explain=" + explain + "]";
 	}
-	
 	
 }
--- src/main/java/com/example/demo/service/UploadService.java
+++ src/main/java/com/example/demo/service/UploadService.java
@@ -1,5 +1,12 @@
 package com.example.demo.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,8 +20,12 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,6 +35,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.dao.UploadDAO;
 import com.example.demo.dto.UploadDTO;
@@ -35,9 +48,33 @@ public class UploadService {
 	UploadDAO upDAO;
 
 	public void uploadGame(UploadDTO upDTO) throws Exception{
-		
+
 		System.out.println(upDTO);
 		upDAO.uploadGame(upDTO);
 	}
-
+	
+	public void fileSet(UploadDTO upDTO, MultipartFile mf, HttpSession session) throws Exception{
+		long time = System.currentTimeMillis();
+		final String path = session.getServletContext().getRealPath("/resources/upload");
+		mf = upDTO.getFiles();
+		
+		System.out.println("time : "+time);
+		System.out.println("path : "+path);
+		System.out.println("mf : "+mf);
+		
+		if(!mf.isEmpty()) {
+			String orgName = mf.getOriginalFilename();
+			String newName = orgName+time+mf.getSize();
+			File files =  new File(path + File.separator+newName);
+			upDTO.setOrgfile(orgName);
+			upDTO.setNewfile(newName);
+			mf.transferTo(files);
+			
+			System.out.println("orgName : "+orgName);
+			System.out.println("newName : "+newName);
+			System.out.println("files : "+files);
+		}
+		
+		upDAO.setNewFile(upDTO);
+	}
 }
