6add6521b0beb7e58f48a2470cf34ea76026e6f2
smlee98 <smlee@gridone.co.kr>
2020-09-20 23:17:32 +0900

    Release 19 (200920_23:17)

--- src/main/java/com/example/demo/security/GetInfo.java
+++ src/main/java/com/example/demo/security/GetInfo.java
@@ -8,45 +8,50 @@ import com.sun.management.OperatingSystemMXBean;
 
 public class GetInfo {
 	
+		public String toGB(long size) {
+			return String.valueOf((int) (size / (1024 * 1024 * 1024)));
+		}
+	
 		public String[] getDiskSpace() {
 			File root = null;
 			try {
 				root = new File("/");
-				String[] list = new String[2];
+				String avgDisk = String.valueOf(100 - (int)(((double)root.getUsableSpace() / (double)root.getTotalSpace()) * 100));
+				String[] list = new String[3];
 				list[0] = toGB(root.getTotalSpace()); 
 				list[1] = toGB(root.getUsableSpace());
+				list[2] = avgDisk;
+				
 				return list;
 			} catch (Exception e) {
 				e.printStackTrace();
 				return null;
 			}
 		}
-	    
-		public String toGB(long size) {
-			return String.valueOf((int) (size / (1024 * 1024 * 1024)));
-		}
 		
 		public String[] getCPUProcess() {
 			
 			OperatingSystemMXBean osbean = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();		
 			String cpuUsage = String.format("%.2f", osbean.getSystemCpuLoad() * 100);
-			String[] list = new String[1];
+			String avgCPU = String.valueOf((int)(osbean.getSystemCpuLoad() * 100));
+			String[] list = new String[2];
 			list[0] = cpuUsage;
+			list[1] = avgCPU;			
 			
 			return list;
 		}
 		
 		public String[] getMemory() {
-			MemoryMXBean membean = (MemoryMXBean) ManagementFactory.getMemoryMXBean();
-			MemoryUsage heap = membean.getHeapMemoryUsage();
-			MemoryUsage nonheap = membean.getNonHeapMemoryUsage();
-
-			long heapUsed = heap.getUsed();
-			long nonheapUsed = nonheap.getUsed();
+			OperatingSystemMXBean osbean = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+			String usableMem = String.format("%.2f", (double)osbean.getFreePhysicalMemorySize() / (1024*1024*1024) );
+			String fullMem = String.format("%.2f", (double)osbean.getTotalPhysicalMemorySize() / (1024*1024*1024) );
+			String avgMem = String.valueOf((int)(((double)osbean.getFreePhysicalMemorySize() / (double)osbean.getTotalPhysicalMemorySize()) * 100));
 			
-			String[] list = new String[2];
-			list[0] = String.valueOf(heapUsed);
-			list[1] = String.valueOf(nonheapUsed);
+			String[] list = new String[3];
+			list[0] = fullMem;
+			list[1] = usableMem;
+			list[2] = avgMem;
+			
 			return list;
 		}
 }
